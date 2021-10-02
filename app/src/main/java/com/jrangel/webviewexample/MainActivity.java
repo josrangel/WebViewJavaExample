7f4cc20bc8e6552cf9245984167d28b54581bd78
Josue <josrangel@hotmail.com>
2021-10-02 16:25:14 -0500

    - add code for buttons

--- app/src/main/java/com/jrangel/webviewexample/MainActivity.java
+++ app/src/main/java/com/jrangel/webviewexample/MainActivity.java
@@ -1,8 +1,11 @@
 package com.jrangel.webviewexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openForIntent(View v) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(Constants.URL_PAGE));
+        startActivity(i);
+    }
+
+    public void openForWebView(View v) {
+        Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+        startActivity(intent);
     }
 }
